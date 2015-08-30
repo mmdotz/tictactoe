aeb01334fb720f3917914e97f10137f7af249e35
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-08-29 19:02:38 -0500

    added computer's turn -> errors

--- spec/testinggame.rb
+++ spec/testinggame.rb
@@ -1,6 +1,9 @@
+
+
 class Player
+
   def initialize
-    @choice_array = []      #didn't need @choice here because we assigned it when first used
+    @choice_array = []    #didn't need @choice here because we assigned it when first used
   end
 
   def choice
@@ -19,17 +22,21 @@ class Player
     puts "What number on the board do you choose?"
     @choice = gets.chomp.to_i
   end
+
+  def random_choice(arg)         #computer chooses number
+    @random_choice
+  end
 end
 
 
 class Board
 
-  attr_reader :spaces, :outcome1, :outcome2, :outcome3, :outcome4, :outcome5, :outcome6, :outcome7, :outcome8
+  attr_reader :spaces, :outcome1, :outcome2, :outcome3, :outcome4, :outcome5, :outcome6, :outcome7, :outcome8  # => nil
 
   def initialize
     @spaces = [0,1,2,3,4,5,6,7,8]
     @outcome1 = [0,1,2]
-    @outcome2 = [3,4,5]       #board.outcome2
+    @outcome2 = [3,4,5]
     @outcome3 = [6,7,8]
     @outcome4 = [0,3,6]
     @outcome5 = [1,4,7]
@@ -44,39 +51,72 @@ class Game
 
   def run_game
 
-    human = Player.new        #made a new player
+    #def decide_players throws an error because can't instatiate new players inside method?
+
+    # def decide_players
+    #   puts "Do you want to play [P]layer vs. Computer OR [C]omputer vs. Computer?"
+    #   game_type = gets.chomp.upcase
+    #   if game_type == "C"     #create 2 computer players
+    #     computer_1 = Player.new
+    #     computer_2 = Player.new
+    #   elsif game_type == "P"   #create human and computer players
+    #     human = Player.new
+    #     computer_1 = Player.new
+    #   else
+    #    puts "Please choose only C or P"
+    #   end
+    # end
+
+    human = Player.new
+    computer = Player.new
     board = Board.new         #created a new board to play on
 
-    until human.choice_array.length == 3
+
+    until human.choice_array.length == 4
     human.choose_b_position   #ask and get
       if board.spaces.include?(human.choice)
         human.build_choice_array
         board.spaces.delete(human.choice)
+        puts "You have chosen #{human.choice_array} so far."
+        computer.random_choice([board.spaces].sample)     #returning nil
+        puts "Available spaces: #{board.spaces}"
+        computer.build_choice_array
+        board.spaces.delete(computer.choice)
+        puts "Computer has taken a turn, choosing #{computer.choice_array}"
+
       else
         puts "Not a valid choice.  Pick only from available spaces :#{board.spaces}"
       end
     end
 
-    #human.choice_array.length
-    print "You have chosen : #{human.choice_array}"
 
     #check against winning array
-    if human.choice_array.eql?(board.outcome1)
-      puts " you won!"
+    if human.choice_array.include?(board.outcome1.inspect)
+      puts "You won!"
     elsif
       human.choice_array.eql?(board.outcome2)
+      puts "You won!"
     elsif
       human.choice_array.eql?(board.outcome3)
+      puts "You won!"
     elsif
       human.choice_array.eql?(board.outcome4)
+      puts "You won!"
     elsif
       human.choice_array.eql?(board.outcome5)
+      puts "You won!"
     elsif
       human.choice_array.eql?(board.outcome6)
+      puts "You won!"
     elsif
       human.choice_array.eql?(board.outcome7)
+      puts "You won!"
     elsif
       human.choice_array.eql?(board.outcome8)
+      puts "You won!"
+    elsif
+      puts "Try another number." # -- need DRAW code here --
+      puts "Its a draw"
     else
       puts "You lose!"
     end
